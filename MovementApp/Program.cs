a59615edc0fe2ce18b20c716670433c26adbea1c
FORTUNE DIAPIM <fadiapim@st.ug.edu.gh>
2025-08-04 15:53:07 +0000

    movement run progress

--- MovementApp/Program.cs
+++ MovementApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MovementApp
+{
+    // Interface definition
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    // Class implementing IMovable: Car
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Class implementing IMovable: Bicycle
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    // Main program
+    class Program
+    {
+        static void Main()
+        {
+            IMovable myCar = new Car();
+            IMovable myBike = new Bicycle();
+
+            myCar.Move();
+            myBike.Move();
+        }
+    }
+}
