eae76a2cd10a2fa9412dc3cbeca1816a6c41592a
FORTUNE DIAPIM <fadiapim@st.ug.edu.gh>
2025-08-04 15:41:16 +0000

    ShapeArea progress

--- AnimalSoundsApp/Program.cs
+++ AnimalSoundsApp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AnimalSoundApp
+{
+    // Base class
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class: Dog
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class: Cat
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    // Main program
+    class Program
+    {
+        static void Main()
+        {
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            genericAnimal.MakeSound(); // Outputs: Some generic sound
+            dog.MakeSound();           // Outputs: Bark
+            cat.MakeSound();           // Outputs: Meow
+        }
+    }
+}
