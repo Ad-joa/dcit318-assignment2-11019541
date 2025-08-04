c4e36209e6c6eb190b46075e3709b5621bd56815
FORTUNE DIAPIM <fadiapim@st.ug.edu.gh>
2025-08-04 15:47:12 +0000

    shape area progress

--- ShapeAreaApp/Program.cs
+++ ShapeAreaApp/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ShapeAreaApp
+{
+    // Abstract base class
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    // Derived class: Circle
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    // Derived class: Rectangle
+    public class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    // Main program
+    class Program
+    {
+        static void Main()
+        {
+            Shape circle = new Circle(5.0);
+            Shape rectangle = new Rectangle(4.0, 6.0);
+
+            Console.WriteLine($"Area of the circle: {circle.GetArea():F2}");
+            Console.WriteLine($"Area of the rectangle: {rectangle.GetArea():F2}");
+        }
+    }
+}
